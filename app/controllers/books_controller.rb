cab693ef142cde5aa54f70ec631e30489e80c6fc
Abby Christens <achristens@gmail.com>
2017-11-03 15:38:52 -0400

    Generate data for each response type page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,18 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+
     respond_to do |format|
       format.html
       format.text
+      format.json do
+        render json: @books
+      end
       format.csv do
         render plain: Book.generate_csv(@books)
       end
     end
+
   end
 
 end
